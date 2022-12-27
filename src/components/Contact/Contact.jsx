e52b35020c9a535b3190129de18dfc61ec2ac373
h0wter <howter99@gmail.com>
2022-12-27 05:03:08 +0200

    add props disabled

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,18 +1,25 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { ContactItem, ContactData } from './Contact.styled';
 import { Button } from 'components/Button/Button';
 import { deleteContact } from 'redux/operations';
+import { getDeletingId } from 'redux/selectors';
 
 export const Contact = ({ contact }) => {
   const dispatch = useDispatch();
   const { name, phone, id } = contact;
+  const deletingId = useSelector(getDeletingId);
+
   return (
     <ContactItem>
       <ContactData>
         {name}: {phone}
       </ContactData>
-      <Button type="button" onClick={() => dispatch(deleteContact(id))}>
+      <Button
+        disabled={id === deletingId}
+        type="button"
+        onClick={() => dispatch(deleteContact(id))}
+      >
         Delete
       </Button>
     </ContactItem>
